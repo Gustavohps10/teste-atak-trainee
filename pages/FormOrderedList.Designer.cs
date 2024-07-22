bb6ab35db89f4ba5f3dde6843263cc0ba1d6617e
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-07-22 08:00:02 -0300

    refactor: add Bunifu textbox in FormOrderedLIst

--- pages/FormOrderedList.Designer.cs
+++ pages/FormOrderedList.Designer.cs
@@ -29,13 +29,17 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormOrderedList));
-            Bunifu.UI.WinForms.BunifuButton.BunifuButton2.BorderEdges borderEdges1 = new Bunifu.UI.WinForms.BunifuButton.BunifuButton2.BorderEdges();
+            Bunifu.UI.WinForms.BunifuButton.BunifuButton2.BorderEdges borderEdges2 = new Bunifu.UI.WinForms.BunifuButton.BunifuButton2.BorderEdges();
+            Bunifu.UI.WinForms.BunifuTextBox.StateProperties stateProperties5 = new Bunifu.UI.WinForms.BunifuTextBox.StateProperties();
+            Bunifu.UI.WinForms.BunifuTextBox.StateProperties stateProperties6 = new Bunifu.UI.WinForms.BunifuTextBox.StateProperties();
+            Bunifu.UI.WinForms.BunifuTextBox.StateProperties stateProperties7 = new Bunifu.UI.WinForms.BunifuTextBox.StateProperties();
+            Bunifu.UI.WinForms.BunifuTextBox.StateProperties stateProperties8 = new Bunifu.UI.WinForms.BunifuTextBox.StateProperties();
             label1 = new Label();
             lbxSortedWords = new ListBox();
-            txtWords = new TextBox();
             lblWords = new Label();
             lblResult = new Label();
             btnSort = new Bunifu.UI.WinForms.BunifuButton.BunifuButton2();
+            txtWords = new Bunifu.UI.WinForms.BunifuTextBox();
             SuspendLayout();
             // 
             // label1
@@ -60,15 +64,6 @@
             lbxSortedWords.Name = "lbxSortedWords";
             lbxSortedWords.Size = new Size(350, 169);
             lbxSortedWords.TabIndex = 2;
-            // 
-            // txtWords
-            // 
-            txtWords.Font = new Font("Segoe UI", 9F);
-            txtWords.Location = new Point(237, 93);
-            txtWords.Name = "txtWords";
-            txtWords.Size = new Size(350, 23);
-            txtWords.TabIndex = 3;
-            txtWords.Text = "Exemplo, Melância, Zebra, Abacaxi, Batata";
             // 
             // lblWords
             // 
@@ -109,11 +104,11 @@
             btnSort.ColorContrastOnClick = 45;
             btnSort.ColorContrastOnHover = 45;
             btnSort.Cursor = Cursors.Hand;
-            borderEdges1.BottomLeft = true;
-            borderEdges1.BottomRight = true;
-            borderEdges1.TopLeft = true;
-            borderEdges1.TopRight = true;
-            btnSort.CustomizableEdges = borderEdges1;
+            borderEdges2.BottomLeft = true;
+            borderEdges2.BottomRight = true;
+            borderEdges2.TopLeft = true;
+            borderEdges2.TopRight = true;
+            btnSort.CustomizableEdges = borderEdges2;
             btnSort.DialogResult = DialogResult.None;
             btnSort.DisabledBorderColor = Color.Transparent;
             btnSort.DisabledFillColor = Color.FromArgb(204, 204, 204);
@@ -137,7 +132,7 @@
             btnSort.IdleIconLeftImage = null;
             btnSort.IdleIconRightImage = null;
             btnSort.IndicateFocus = false;
-            btnSort.Location = new Point(237, 131);
+            btnSort.Location = new Point(237, 138);
             btnSort.Name = "btnSort";
             btnSort.OnDisabledState.BorderColor = Color.Transparent;
             btnSort.OnDisabledState.BorderRadius = 8;
@@ -180,16 +175,94 @@
             btnSort.UseDefaultRadiusAndThickness = true;
             btnSort.Click += btnSortList_Click;
             // 
+            // txtWords
+            // 
+            txtWords.AcceptsReturn = false;
+            txtWords.AcceptsTab = false;
+            txtWords.AnimationSpeed = 200;
+            txtWords.AutoCompleteMode = AutoCompleteMode.None;
+            txtWords.AutoCompleteSource = AutoCompleteSource.None;
+            txtWords.AutoSizeHeight = true;
+            txtWords.BackColor = Color.Transparent;
+            txtWords.BackgroundImage = (Image)resources.GetObject("txtWords.BackgroundImage");
+            txtWords.BorderColorActive = Color.DodgerBlue;
+            txtWords.BorderColorDisabled = Color.FromArgb(204, 204, 204);
+            txtWords.BorderColorHover = Color.FromArgb(105, 181, 255);
+            txtWords.BorderColorIdle = Color.Silver;
+            txtWords.BorderRadius = 1;
+            txtWords.BorderThickness = 1;
+            txtWords.CharacterCase = Bunifu.UI.WinForms.BunifuTextBox.CharacterCases.Normal;
+            txtWords.CharacterCasing = CharacterCasing.Normal;
+            txtWords.DefaultFont = new Font("Segoe UI", 9.25F);
+            txtWords.DefaultText = "Exemplo, Melância, Zebra, Abacaxi, Batata";
+            txtWords.FillColor = Color.White;
+            txtWords.HideSelection = true;
+            txtWords.IconLeft = null;
+            txtWords.IconLeftCursor = Cursors.IBeam;
+            txtWords.IconPadding = 10;
+            txtWords.IconRight = null;
+            txtWords.IconRightCursor = Cursors.IBeam;
+            txtWords.Lines = new string[]
+    {
+    "Exemplo, Melância, Zebra, Abacaxi, Batata"
+    };
+            txtWords.Location = new Point(237, 93);
+            txtWords.MaxLength = 32767;
+            txtWords.MinimumSize = new Size(1, 1);
+            txtWords.Modified = false;
+            txtWords.Multiline = false;
+            txtWords.Name = "txtWords";
+            stateProperties5.BorderColor = Color.DodgerBlue;
+            stateProperties5.FillColor = Color.Empty;
+            stateProperties5.ForeColor = Color.Empty;
+            stateProperties5.PlaceholderForeColor = Color.Empty;
+            txtWords.OnActiveState = stateProperties5;
+            stateProperties6.BorderColor = Color.FromArgb(204, 204, 204);
+            stateProperties6.FillColor = Color.FromArgb(240, 240, 240);
+            stateProperties6.ForeColor = Color.FromArgb(109, 109, 109);
+            stateProperties6.PlaceholderForeColor = Color.DarkGray;
+            txtWords.OnDisabledState = stateProperties6;
+            stateProperties7.BorderColor = Color.FromArgb(105, 181, 255);
+            stateProperties7.FillColor = Color.Empty;
+            stateProperties7.ForeColor = Color.Empty;
+            stateProperties7.PlaceholderForeColor = Color.Empty;
+            txtWords.OnHoverState = stateProperties7;
+            stateProperties8.BorderColor = Color.Silver;
+            stateProperties8.FillColor = Color.White;
+            stateProperties8.ForeColor = Color.Empty;
+            stateProperties8.PlaceholderForeColor = Color.Empty;
+            txtWords.OnIdleState = stateProperties8;
+            txtWords.Padding = new Padding(3);
+            txtWords.PasswordChar = '\0';
+            txtWords.PlaceholderForeColor = Color.Silver;
+            txtWords.PlaceholderText = "Enter text";
+            txtWords.ReadOnly = false;
+            txtWords.ScrollBars = ScrollBars.None;
+            txtWords.SelectedText = "";
+            txtWords.SelectionLength = 0;
+            txtWords.SelectionStart = 41;
+            txtWords.ShortcutsEnabled = true;
+            txtWords.Size = new Size(350, 39);
+            txtWords.Style = Bunifu.UI.WinForms.BunifuTextBox._Style.Bunifu;
+            txtWords.TabIndex = 19;
+            txtWords.TextAlign = HorizontalAlignment.Left;
+            txtWords.TextMarginBottom = 0;
+            txtWords.TextMarginLeft = 3;
+            txtWords.TextMarginTop = 1;
+            txtWords.TextPlaceholder = "Enter text";
+            txtWords.UseSystemPasswordChar = false;
+            txtWords.WordWrap = true;
+            // 
             // FormOrderedList
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(251, 251, 251);
             ClientSize = new Size(800, 450);
+            Controls.Add(txtWords);
             Controls.Add(btnSort);
             Controls.Add(lblResult);
             Controls.Add(lblWords);
-            Controls.Add(txtWords);
             Controls.Add(lbxSortedWords);
             Controls.Add(label1);
             Name = "FormOrderedList";
@@ -202,9 +275,9 @@
 
         private Label label1;
         private ListBox lbxSortedWords;
-        private TextBox txtWords;
         private Label lblWords;
         private Label lblResult;
         private Bunifu.UI.WinForms.BunifuButton.BunifuButton2 btnSort;
+        private Bunifu.UI.WinForms.BunifuTextBox txtWords;
     }
 }
